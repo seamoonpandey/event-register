c14bc009508911c68a86e2b5fbdc3dd2c430a872
Seamoon Pandey <hackedasocial@gmail.com>
2024-01-29 21:05:17 +0545

    add user routes

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import connectDb from "./config/db.js";
 import eventRoutes from "./routes/eventRoutes.js";
 import participantRoutes from "./routes/participantRoutes.js";
 import attendanceRoutes from "./routes/attendanceRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const port = process.env.PORT || 5000;
 
@@ -33,6 +34,7 @@ app.get("/", (req, res) => {
 app.use("/api/events", eventRoutes);
 app.use("/api/participants", participantRoutes);
 app.use("/api/attendance", attendanceRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
