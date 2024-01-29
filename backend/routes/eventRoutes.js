ff2fd9445d95b30759184dc09507f6905dec9b48
Seamoon Pandey <hackedasocial@gmail.com>
2024-01-29 07:54:39 +0545

    update import eventRoutes.js file

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import { getEvents, getEventById } from '../controllers/eventController';
+import express from "express";
+import { getEvents, getEventById } from "../controllers/eventController.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.route('/').get(getEvents);
-router.route('/:id').get(getEventById);
+router.route("/").get(getEvents);
+router.route("/:id").get(getEventById);
 
 export default router;
